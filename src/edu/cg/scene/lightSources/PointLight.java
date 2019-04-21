4cdd84c3e83e039f9f6d229e8f6c9e1567d72ae2
talm <talm@wustl.edu>
2019-04-21 11:40:28 +0300

    added implementation of point light

--- src/edu/cg/scene/lightSources/PointLight.java
+++ src/edu/cg/scene/lightSources/PointLight.java
@@ -1,5 +1,6 @@
 package edu.cg.scene.lightSources;
 
+import edu.cg.algebra.Hit;
 import edu.cg.algebra.Point;
 import edu.cg.algebra.Ray;
 import edu.cg.algebra.Vec;
@@ -33,17 +34,25 @@ public class PointLight extends Light {
 
     @Override
     public Ray rayToLight(Point fromPoint) {
-        return null;
+		return new Ray(fromPoint, position);
     }
 
     @Override
     public boolean isOccludedBy(Surface surface, Ray rayToLight) {
-        return false;
+		Hit hit = surface.intersect(rayToLight);
+		// in case there is no intersection
+		if (hit == null) return false;
+		Point hittingPoint = rayToLight.getHittingPoint(hit);
+		Point sourceOfRayToLight = rayToLight.source();
+		// check if the surface is behind the PointLight
+		return sourceOfRayToLight.distSqr(position) > sourceOfRayToLight.distSqr(hittingPoint);
     }
 
     @Override
     public Vec intensity(Point hittingPoint, Ray rayToLight) {
-        return null;
+		double d = hittingPoint.dist(position);
+		double denominator = (1.0 / kc + kl * d + kq * Math.pow(d, 2));
+		return intensity.mult(denominator);
     }
 
     public PointLight initPosition(Point position) {
